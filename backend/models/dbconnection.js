25727cea6f4e7b43b03b2a80566753486492c19a
Ashrafu <ashraftuyubahe001@gmail.com>
2024-06-09 11:25:23 +0200

    new updates

--- backend/models/dbconnection.js
+++ backend/models/dbconnection.js
@@ -1,16 +1,18 @@
+const mongoose = require("mongoose");
 
-
-const mongoose=require('mongoose');
-
- const dbconnection= mongoose.connect('mongodb://localhost:27017/Ashify', {
+const dbconnection = mongoose
+  .connect("mongodb://localhost:27017/Ashify", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log('Successfully connected to MongoDB');
+    console.log("Successfully connected to MongoDB");
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
+    console.error("Error connecting to MongoDB:", error.message);
   });
 
-module.exports= dbconnection;
+// password = "v6KLLc9EoqzyanmX";
+// const mongodb_URL = "mongodb+srv://tuyubaheashrafu2023:v6KLLc9EoqzyanmX@cluster0.r9bxg1y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+module.exports = dbconnection;
